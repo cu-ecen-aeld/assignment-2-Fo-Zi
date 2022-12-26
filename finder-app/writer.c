4a94b89369edd227c6365f65c28badf03f0ece33
FZ <85625336+Fo-Zi@users.noreply.github.com>
2022-12-26 01:09:52 -0300

    Update writer.c

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -5,21 +5,21 @@
 
 int main(int argc, char* argv[])
 {
-	openlog("writer", LOG_PID, LOG_USER);
+	openlog("Writer", LOG_PID, LOG_USER);
 
 	if (argc != 3) {
-		syslog(LOG_ERR, "Need two arguments.");
+		syslog(LOG_ERR, "The user must input two arguments.");
 		return 1;
 	}
 
 	FILE *f = fopen(argv[1], "wb");
 	if (f == NULL) {
-		syslog(LOG_ERR, "Cannot create file.");
+		syslog(LOG_ERR, "Not able to create/open the file.");
 		return 1;
 	}
 
 	fwrite(argv[2], 1, strlen(argv[2]), f);
-	syslog(LOG_DEBUG, "Writing %s to %s", argv[1], argv[2]);
+	syslog(LOG_DEBUG, "Writing: %s . To: %s ...", argv[1], argv[2]);
 	
 	fclose(f);
 	closelog();
